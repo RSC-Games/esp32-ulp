8bd2bc1be4c34a68d088fa5f45170b8b1654472e
RSC-Games <75383943+RSC-Games@users.noreply.github.com>
2022-06-20 08:32:57 -0400

    Add logging to ld.

--- esp32_ulp/link.py
+++ esp32_ulp/link.py
@@ -1,11 +1,19 @@
+"""
+The ESP32_ULP linker. 
+
+This does not link a bunch of object files together
+but only builds the final binary.
+"""
+
+import log_sys
 from uctypes import struct, addressof, LITTLE_ENDIAN, UINT16, UINT32
 
 
 def make_binary(text, data, bss_size):
     if not isinstance(text, bytes):
-        raise TypeError('text section must be binary bytes')
+        log_sys.log_e("ulp_ld", "Invalid .text region: .text section must be binary bytes.")
     if not isinstance(data, bytes):
-        raise TypeError('data section must be binary bytes')
+        log_sys.log_e("ulp_ld", "Invalid .data region: .data section must be binary bytes.")
     binary_header_struct_def = dict(
         magic = 0 | UINT32,
         text_offset = 4 | UINT16,
@@ -22,5 +30,11 @@ def make_binary(text, data, bss_size):
     h.text_size = len(text)  # size of .text section (2 bytes)
     h.data_size = len(data)  # size of .data section (2 bytes)
     h.bss_size = bss_size  # size of .bss section (2 bytes)
+    
+    # Log the binary size.
+    log_sys.log_i("ulp_ld", ".text region size: " + str(len(text)) + " B")
+    log_sys.log_i("ulp_ld", ".data region size: " + str(len(text)) + " B")
+    
+    # Return the final image.
     return bytes(header) + text + data
 
